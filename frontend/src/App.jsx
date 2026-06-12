8690b34cbd37c09d29286a795c133bc7218c2004
Domi <dominique.teillier24@gmail.com>
2026-06-12 16:42:10 -0400

    cambios iniciales de estilos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,22 @@ import LoginPage from './pages/LoginPage'
 import MenuPage from './pages/MenuPage'
 import ProtectedRoute from './components/ProtectedRoute'
 import CategoriaPage from './pages/CategoriaPage'
+import DashbdPage from './pages/DashbdPage/DashbdPage'
+import CitasPage from './pages/CitasPage/CitasPage'
+import DuenosPage from './pages/DuenosPage/DuenosPage'
+import MascotasPage from './pages/MascotasPage/MascotasPage'
+import VetPage from './pages/VetPage/VetPage'
+import TratamientosPage from './pages/TratamientosPage/TratamientosPage'
 
 const route = createBrowserRouter([
   {path:'/', element:<LoginPage />},
   {path:'/menu', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><MenuPage/></ProtectedRoute>},
-  {path:'/categoria', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><CategoriaPage/></ProtectedRoute>},
+  {path:'/dashboard', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><DashbdPage/></ProtectedRoute>},
+  {path:'/citas', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><CitasPage/></ProtectedRoute>},
+  {path:'/duenos', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><DuenosPage/></ProtectedRoute>},
+  {path:'/mascotas', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN","ROLE_USER"]}><MascotasPage/></ProtectedRoute>},
+  {path:'/veterinarios', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN"]}><VetPage/></ProtectedRoute>},
+  {path:'/tratamientos', element:<ProtectedRoute allowedRoles={["ROLE_ADMIN"]}><TratamientosPage/></ProtectedRoute>},
   {path:'/no-autorizado', element:<h3>No tienes permisos para acceder a esta página</h3>}
 
 ])
--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -11,8 +11,10 @@ function LoginPage(){
 
     const loginAction = async()=>{
         console.log('login')
+        navigate('/menu')
         const resp = await loginApi({rut:rut, password:password})
         console.log(resp)
+        navigate('/menu')
         if(resp?.token){
             await saveToken(resp.token)
             navigate('/menu', {replace:true})
@@ -24,11 +26,21 @@ function LoginPage(){
 
 
     return <>
-        <h3>Login</h3>
-        <input type="text" onChange={(e)=> setRut(e.target.value)} />
-        <input type="password" onChange={(e)=> setPassword(e.target.value)} />
-        <button onClick={loginAction}>login</button>
-
+        <div className="flex flex-col items-center justify-center w-full min-h-screen py-16 sm:py-20 px-10 sm:px-6 lg:px-8 relative bg-bg-vg">
+            <div className="flex flex-col items-center justify-center pb-5">
+                <img></img>
+                <span className="text-2xl font-bold"> VetGo</span>
+                <span className="text-md text-gray-400"> Ingrese a su cuenta</span>
+            </div>
+            
+            <div className="p-3 relative flex flex-col items-start justify-center bg-white border border-gray-100 rounded-lg shadow-sm shadow-black/20 sm:rounded-2xl sm:p-6 z-10">
+                <span className="text-sm text-gray-700 font-semibold ">Correo Electrónico</span>
+                <input type="text" onChange={(e)=> setRut(e.target.value)} />
+                <span className="text-sm text-gray-700 font-semibold">Contraseña</span>
+                <input type="password" onChange={(e)=> setPassword(e.target.value)} />
+                <button onClick={loginAction}>login</button>
+        </div>
+        </div>
 
     </>
 }
