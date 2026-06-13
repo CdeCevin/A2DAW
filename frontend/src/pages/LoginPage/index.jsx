9d788051ee0e7f94dde3d7b7d7f513a0fb8ba020
Dominique Teillier <dominique.teillier24@outlook.com>
2026-06-13 00:18:54 -0400

    arreglado: tailwind. Añadido:heroui. Avance: mini login

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -2,16 +2,16 @@ import { useContext, useState } from "react";
 import { loginApi } from "../../api";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../store/auth-context";
+import {Button, Card, Form, Input, Label, Link, TextField} from "@heroui/react";
 
 function LoginPage(){
-    const [rut, setRut] = useState('');
+    const [correo, setCorreo] = useState('');
     const [password, setPassword] = useState('')
     const navigate = useNavigate();
     const {saveToken} = useContext(AuthContext)
 
     const loginAction = async()=>{
         console.log('login')
-        navigate('/menu')
         const resp = await loginApi({rut:rut, password:password})
         console.log(resp)
         navigate('/menu')
@@ -25,8 +25,9 @@ function LoginPage(){
     }
 
 
-    return <>
+    return (
         <div className="flex flex-col items-center justify-center w-full min-h-screen py-16 sm:py-20 px-10 sm:px-6 lg:px-8 relative bg-bg-vg">
+            {/* 
             <div className="flex flex-col items-center justify-center pb-5">
                 <img></img>
                 <span className="text-2xl font-bold"> VetGo</span>
@@ -39,10 +40,35 @@ function LoginPage(){
                 <span className="text-sm text-gray-700 font-semibold">Contraseña</span>
                 <input type="password" onChange={(e)=> setPassword(e.target.value)} />
                 <button onClick={loginAction}>login</button>
-        </div>
-        </div>
+        </div>*/}
+        
 
-    </>
-}
+        <Card className="w-full max-w-md bg-white">
+        <Card.Header>
+            <Card.Title>Login</Card.Title>
+            <Card.Description>Enter your credentials to access your account</Card.Description>
+        </Card.Header>
+        <Form onSubmit={loginAction}>
+            <Card.Content>
+            <div className="flex flex-col gap-4">
+                <TextField name="email" type="email" type="text" onChange={(e)=> setCorreo(e.target.value)}>
+                <Label>Correo Electronico</Label>
+                <Input placeholder="correo@ejemplo.com" variant="secondary" />
+                </TextField>
+                <TextField name="password" type="password" onChange={(e)=> setPassword(e.target.value)}>
+                <Label>Contraseña</Label>
+                <Input placeholder="••••••••" variant="secondary" />
+                </TextField>
+            </div>
+            </Card.Content>
+            <Card.Footer className="mt-4 flex flex-col gap-2">
+            <Button className="w-full bg-aqua-vg" type="submit">
+                Ingresar
+            </Button>
+            </Card.Footer>
+        </Form>
+        </Card>
+    </div>
+);}
 
 export default LoginPage;
