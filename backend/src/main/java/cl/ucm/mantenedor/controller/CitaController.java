5282cd492fab5c0837e88df4488dd4d666fff521
CdeCevin <kolivaresfuentes@gmail.com>
2026-06-30 03:22:35 -0400

    feat: use 201/204 status codes and add global exception handler for empty fields and db constraints

--- backend/src/main/java/cl/ucm/mantenedor/controller/CitaController.java
+++ backend/src/main/java/cl/ucm/mantenedor/controller/CitaController.java
@@ -154,7 +154,7 @@ public class CitaController {
     public ResponseEntity<?> delete(@PathVariable Integer id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
-            return ResponseEntity.ok("Cita eliminada con éxito");
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
