5293131dfad140714ceceb399d03327c665a8195
CdeCevin <kolivaresfuentes@gmail.com>
2026-06-30 18:20:25 -0400

    Print limpio

--- backend/src/main/java/cl/ucm/mantenedor/MantenedorApplication.java
+++ backend/src/main/java/cl/ucm/mantenedor/MantenedorApplication.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class MantenedorApplication {
 
 	public static void main(String[] args) {
-		System.out.println("BCRYPT HASH FOR 123456789: " + new BCryptPasswordEncoder().encode("123456789"));
 		SpringApplication.run(MantenedorApplication.class, args);
 	}
 
